0b045c40ece163d6aad066988693eb7d9c0a740f
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-05-28 15:38:50 +0900

    add thirdweb engine client

--- backend/server/src/modules/gasha/gasha.controller.ts
+++ backend/server/src/modules/gasha/gasha.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Logger, Post } from '@nestjs/common';
+import { Controller, Get, Logger, Post } from '@nestjs/common';
 import { NeynarService } from '../neynar/neynar.service';
 import { ViemService } from '../viem/viem.service';
+import tweClient from 'src/lib/thirdwebEngine';
 
 @Controller('gasha')
 export class GashaController {
@@ -9,6 +10,28 @@ export class GashaController {
     private readonly neynarService: NeynarService,
     private readonly viemService: ViemService,
   ) {}
+
+  @Get('/engine/test')
+  async test() {
+    this.logger.log(this.test.name);
+
+    const { data } = await tweClient.GET(
+      '/contract/{chain}/{contractAddress}/read',
+      {
+        params: {
+          query: {
+            functionName: 'seriesItems',
+          },
+          path: {
+            chain: '666666666',
+            contractAddress: '0x409467ad8Ca45eA5626747c6538AbCACb7e0A292',
+          },
+        },
+      },
+    );
+
+    return data;
+  }
 
   @Post('/engine/spin')
   async spinGashaEngine() {
