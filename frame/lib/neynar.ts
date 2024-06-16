8124fcc0f6fcb1543fdffddd2fbf7e83403c90f7
てんでん <barururu3.pororo.kmt@gmail.com>
2024-06-16 21:16:06 +0900

    レスポンス速度改善のため、各処理の共通化と並列化を実施

--- frame/lib/neynar.ts
+++ frame/lib/neynar.ts
@@ -1,18 +1,16 @@
 import { NEYNAR_API_KEY } from '../constant/config.js';
 
 export const getFarcasterUserInfo = async (fid?: number) => {
-  const userInfo = await fetch(
+  const response = await fetch(
     `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}&viewer_fid=3`,
     {
       method: 'GET',
       headers: { accept: 'application/json', api_key: NEYNAR_API_KEY },
     },
   );
-  const userData = await userInfo.json();
-  const pfp_url = userData.users[0].pfp_url;
-  const userName = userData.users[0].username;
-  const verifiedAddresses = userData.users[0].verified_addresses.eth_addresses;
-
+  const { users } = await response.json();
+  const { pfp_url, username: userName, verified_addresses } = users[0];
+  const verifiedAddresses = verified_addresses.eth_addresses;
   return { pfp_url, userName, verifiedAddresses };
 };
 
