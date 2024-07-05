bf506f78dcf8746278e3a1523b4c61388d8e8ea9
kunroku <es-shirahama+shirahama@eversystem.co.jp>
2024-07-06 00:45:41 +0900

    reward データ取得効率化

--- batch/src/saveRewardHistoryData.ts
+++ batch/src/saveRewardHistoryData.ts
@@ -10,23 +10,20 @@ import StackVariablesABI from './abi/StackVariables.json';
 const MAX_REWARD_AMOUNT_RATIO = 190;
 
 export const handler = async (year: number, month: number, day: number) => {
-  const historyFile = null;
-  // const historyFile = await getFileFromS3('reward_history/history.json');
+  // const historyFile = null;
+  const historyFile = await getFileFromS3('reward_history/history.json');
   const history: RewardHistoryData[] = historyFile || [];
 
   const startdate = Math.floor(Date.UTC(year, month - 1, day - 1, 3) / 1e3);
   const enddate = startdate + (24 * 60 * 60);
   const beforedate = startdate - (24 * 60 * 60);
-  const res = await fetch(
-    `${API_ENDPOINT}/points/calcurate-score?end_date_unix=${enddate * 1e3}`
-  );
-  const resData = (await res.json()) as [Address, number][];
-  // console.log(resData);
-  const totalStack = resData.map((e) => e[1]).reduce((a, b) => a + b);
-  const battleLogs = await getGameRevealedLogs(beforedate, startdate);
-  // console.log(battleLogs);
+  const [calcuratedScores, battleLogs] = await Promise.all([
+    fetch(`${API_ENDPOINT}/points/calcurate-score?end_date_unix=${enddate * 1e3}`)
+      .then(res => res.json() as unknown as Promise<[Address, number][]>),
+    getGameRevealedLogs(beforedate, startdate)
+  ]);
+  const totalStack = calcuratedScores.map((e) => e[1]).reduce((a, b) => a + b);
   const maxRewardAmount = battleLogs.length * MAX_REWARD_AMOUNT_RATIO;
-
   const contract = getContract({
     address: STACK_VARIABLES_CONTRACT_ADDRESS as Address,
     abi: StackVariablesABI,
@@ -56,7 +53,7 @@ export const handler = async (year: number, month: number, day: number) => {
     totalStack,
     date: `${year}/${`00${month}`.slice(-2)}/${`00${day}`.slice(-2)}`,
   });
-  console.log(history);
+  // console.log(history);
   // ToDo: ファイル名変更
   await uploadS3(
     history,
