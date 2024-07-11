1c5cb1f9fc4d4785f3afd408f58e438a60ad7990
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-07-11 17:32:05 +0900

    deploy

--- contract/scripts/upgrade/main.ts
+++ contract/scripts/upgrade/main.ts
@@ -2,8 +2,8 @@ import { upgradeGashaContract } from '../helper/gasha';
 import { upgradeWarContract, upgradeWarPoolContract } from '../helper/war';
 
 const main = async () => {
-  // await upgradeWarContract(process.env.WAR_CONTRACT_ADDRESS!);
-  // await new Promise((resolve) => setTimeout(resolve, 2000));
+  await upgradeWarContract(process.env.WAR_CONTRACT_ADDRESS!);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   // await upgradeWarPoolContract(process.env.WAR_POOL_CONTRACT_ADDRESS!);
   // await new Promise((resolve) => setTimeout(resolve, 2000));
   await upgradeGashaContract(process.env.GASHA_CONTRACT_ADDRESS!);
